ff9eacc703eed0a2f0132b5d8d0ab2228e55bb66
DuyVu <vuvanduy06122000@gmail.com>
2024-05-12 01:02:46 +0700

    Day_21

--- Day_21/js/bai_4.js
+++ Day_21/js/bai_4.js
@@ -1,5 +1,6 @@
 var spans = document.querySelectorAll(".lesson-four");
 var numbers = Array.from({ length: 10 }, () => Math.floor(Math.random() * 10));
+// var numbers = [2, 5, 23, 8, 3, 10];
 var numberAdd = 6;
 var sortArr = [];
 var newNumbers = [];
@@ -16,27 +17,45 @@ function sortArray(arr) {
   }
   return arr;
 }
-for (var value of numbers) {
-  sortArr[sortArr.length] = value;
-}
-sortArr = sortArray(sortArr);
 
-spans[0].innerHTML = numberAdd;
-spans[1].innerHTML = `[${numbers}]`;
-spans[2].innerHTML = `[${sortArr}]`;
-
-if (numberAdd <= sortArr[0]) {
-  var lastSortArr = [numberAdd];
-  for (var value of sortArr) {
-    lastSortArr[lastSortArr.length] = value;
+(function () {
+  for (var value of numbers) {
+    sortArr[sortArr.length] = value;
   }
-  sortArr = lastSortArr;
-  spans[3].innerHTML = `[${sortArr}]`;
-} else if (numberAdd >= sortArr[sortArr.length - 1]) {
-  sortArr[sortArr.length] = numberAdd;
-  spans[3].innerHTML = `[${sortArr}]`;
-} else {
-  sortArr[sortArr.length] = numberAdd;
   sortArr = sortArray(sortArr);
-  spans[3].innerHTML = `[${sortArr}]`;
-}
+
+  spans[0].innerHTML = numberAdd;
+  spans[1].innerHTML = `[${numbers}]`;
+  spans[2].innerHTML = `[${sortArr}]`;
+
+  if (numberAdd <= sortArr[0]) {
+    var lastSortArr = [numberAdd];
+    for (var value of sortArr) {
+      lastSortArr[lastSortArr.length] = value;
+    }
+    sortArr = lastSortArr;
+    spans[3].innerHTML = `[${sortArr}]`;
+  } else if (numberAdd >= sortArr[sortArr.length - 1]) {
+    sortArr[sortArr.length] = numberAdd;
+    spans[3].innerHTML = `[${sortArr}]`;
+  } else {
+    for (var index in sortArr) {
+      if (numberAdd <= sortArr[index]) {
+        var temp_sortArr = [];
+        for (var index1 in sortArr) {
+          if (index1 === index) {
+            temp_sortArr[temp_sortArr.length] = numberAdd;
+            temp_sortArr[temp_sortArr.length] = sortArr[index1];
+          } else {
+            temp_sortArr[temp_sortArr.length] = sortArr[index1];
+          }
+        }
+        sortArr = temp_sortArr;
+        break;
+      }
+    }
+    // sortArr[sortArr.length] = numberAdd;
+    // sortArr = sortArray(sortArr);
+    spans[3].innerHTML = `[${sortArr}]`;
+  }
+})();
